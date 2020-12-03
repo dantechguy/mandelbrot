46efc38cd6dcb130d485e733458359c15c2366d1
Daniel Wendon-Blixrud <d@nielwb.com>
2020-12-03 19:27:19 +0000

    added gradient

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ def converges(c):
             return False
     return True
 
+def converge_count(c):
+    z = 0.0
+    for i in range(200):
+        z = z**2 + c
+        if math.sqrt(z.real**2 + z.imag**2) >= 2:
+            return i
+    return 0
+
 def coords_to_complex(x, y):
     return complex(
         (x - width//2) / unit_size,
@@ -28,9 +36,18 @@ def complex_to_coords(z):
         (z.imag * unit_size) + height//2
     )
 
-for y in range(height):
-    for x in range(width):
-        if converges(coords_to_complex(x, y)):
-            canvas.create_rectangle( (x, y)*2 )
+def run():
+    for y in range(height):
+        for x in range(width):
+            # if converges(coords_to_complex(x, y)):
+            val = min(converge_count(coords_to_complex(x, y))*10, 255)
+            # if val != 0:
+            #     print(val)
+            col = '#%02x%02x%02x' % (val, val, val)
+            # print(col)
+            canvas.create_rectangle( (x, y)*2, fill=col, width=0)
+            # root.update()
 
+run()
+# root.after(100, run)
 root.mainloop()
